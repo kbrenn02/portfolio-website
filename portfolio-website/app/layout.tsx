9708dd8ad178f34e88353ac9c7e94df23e4e05ad
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-06-07 08:54:32 -0700

    Created footer and added it to layout

--- portfolio-website/app/layout.tsx
+++ portfolio-website/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
 import Navbar from "@/src/components/Navbar";
+import Footer from "@/src/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         <main className="mx-auto max-w-3xl px-3 py-10">{children}</main>
+        <Footer />
       </body>
     </html>
   );
---
+++ portfolio-website/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Footer = () => {
+  return <footer>
+    <nav className='max-w-3xl mx-auto flex gap-3 p-3'>
+        <Link href="/privacy">Privacy Policy</Link>
+    </nav>
+  </footer>
+}
+
+export default Footer
