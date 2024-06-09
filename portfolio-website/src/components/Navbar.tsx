67e929cbc3cd8a90cce4a14f28240d9b9cab28e8
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-06-08 17:15:40 -0700

    Built the button to toggle between light and dark

--- portfolio-website/src/components/Navbar.tsx
+++ portfolio-website/src/components/Navbar.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link"
+import ThemeToggle from "./ThemeToggle"
 
 export default function Navbar() {
     // header itself
     return <header className="sticky top-0 bg-background">
         {/* content of the header, which is centered and that wrap*/}
+        {/* With the 'justify between' it means if you have 2 elements, it will put the first element to the left and second to the right */}
         <div className="max-w-3xl mx-auto flex flex-wrap justify-between gap-3 px-3 py-4">
             <nav className="space-x-4 font-medium">
                 {/* recommend single page if using free OpenAI account */}
@@ -12,6 +14,9 @@ export default function Navbar() {
                 <Link href="/projects">Projects</Link>
                 <Link href="/social">Social Media</Link>
             </nav>
+            <div>
+                <ThemeToggle />
+            </div>
         </div>
     </header>
 }
--- portfolio-website/src/components/ThemeToggle.tsx
+++ portfolio-website/src/components/ThemeToggle.tsx
@@ -1,2 +1,22 @@
 'use client';
+// Need to make this a use client because we need to use state to capture the client's button client
+// useTheme is similar to useState and this could be done with useState, but using theme is easier
 
+import React from 'react';
+import { Moon, Sun } from 'lucide-react'
+import { useTheme } from 'next-themes';
+
+
+const ThemeToggle = () => {
+
+    const {theme, setTheme} = useTheme();
+
+    return <button
+    onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+        
+        <Sun size={24} className='absolute transition-all rotate-90 scale-0 dark:rotate-0 dark:scale-100' />
+        <Moon size={24} className='transition-all rotate-0 scale-100 dark:-rotate-90 dark:scale-0' />
+    </button>
+}
+
+export default ThemeToggle
