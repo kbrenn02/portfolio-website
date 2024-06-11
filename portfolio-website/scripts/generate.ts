773e2a6f7e5498e258d176027da14fdf67c01f11
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-06-11 15:41:20 -0700

    Had to un comment some files as a way to troubleshoot vercel deployment

--- portfolio-website/scripts/generate.ts
+++ portfolio-website/scripts/generate.ts
@@ -1,55 +1,55 @@
-// import dotenv from 'dotenv'
-// dotenv.config({path: ".env.local"})
-// // Configure .env before other imports
-// import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
-// import { TextLoader } from "langchain/document_loaders/fs/text";
-// import { DocumentInterface } from "@langchain/core/documents"
-// import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-// import { getEmbeddingsCollection, getVectorStore } from "../src/libs/astradb";
-// import { Redis } from '@upstash/redis';
+import dotenv from 'dotenv'
+dotenv.config({path: ".env.local"})
+// Configure .env before other imports
+import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
+import { TextLoader } from "langchain/document_loaders/fs/text";
+import { DocumentInterface } from "@langchain/core/documents"
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { getEmbeddingsCollection, getVectorStore } from "../src/libs/astradb";
+import { Redis } from '@upstash/redis';
 
-// async function generateEmbeddings() {
+async function generateEmbeddings() {
 
-//     await Redis.fromEnv().flushdb();
+    await Redis.fromEnv().flushdb();
 
-//     const vectorStore = await getVectorStore();
+    const vectorStore = await getVectorStore();
 
-//     (await getEmbeddingsCollection()).deleteMany({});
+    (await getEmbeddingsCollection()).deleteMany({});
 
-//     const loader = new DirectoryLoader(
-//         "app/", 
-//         {
-//             // load the .tsx file (this only takes file extensions) aka all the pages of the website, as a string
-//             ".tsx": (path) => new TextLoader(path)
-//         },
-//         true
-//     )
+    const loader = new DirectoryLoader(
+        "app/", 
+        {
+            // load the .tsx file (this only takes file extensions) aka all the pages of the website, as a string
+            ".tsx": (path) => new TextLoader(path)
+        },
+        true
+    )
 
-//     const docs = (await loader.load())
-//     .filter(doc => doc.metadata.source.endsWith("page.tsx"))
-//     .map((doc): DocumentInterface => {
-//         // This changes the metadata path in my file to the url extension that brings to this page
-//         const url = doc.metadata.source
-//             .split("/app")[1]
-//             .split("/page.")[0] || "/"
+    const docs = (await loader.load())
+    .filter(doc => doc.metadata.source.endsWith("page.tsx"))
+    .map((doc): DocumentInterface => {
+        // This changes the metadata path in my file to the url extension that brings to this page
+        const url = doc.metadata.source
+            .split("/app")[1]
+            .split("/page.")[0] || "/"
 
-//         const pageContentTrimmed = doc.pageContent
-//             .replace(/^import.*$/gm, "") // remove all import statements
-//             .replace(/ className=(["']).*?\1| className{.*?}/g, "") // Remove all className props
-//             .replace(/^\s*[\r]/gm, "") // remove empty lines
-//             .trim();
+        const pageContentTrimmed = doc.pageContent
+            .replace(/^import.*$/gm, "") // remove all import statements
+            .replace(/ className=(["']).*?\1| className{.*?}/g, "") // Remove all className props
+            .replace(/^\s*[\r]/gm, "") // remove empty lines
+            .trim();
 
-//             return {
-//                 pageContent: pageContentTrimmed,
-//                 metadata: { url }
-//             };
-//     });
+            return {
+                pageContent: pageContentTrimmed,
+                metadata: { url }
+            };
+    });
 
-//     const splitter = RecursiveCharacterTextSplitter.fromLanguage("html");
+    const splitter = RecursiveCharacterTextSplitter.fromLanguage("html");
 
-//     const splitDocs = await splitter.splitDocuments(docs)
+    const splitDocs = await splitter.splitDocuments(docs)
 
-//     await vectorStore.addDocuments(splitDocs);
-// }
+    await vectorStore.addDocuments(splitDocs);
+}
 
-// generateEmbeddings();
+generateEmbeddings();
--- portfolio-website/src/libs/astradb.ts
+++ portfolio-website/src/libs/astradb.ts
@@ -1,35 +1,35 @@
-// import { AstraDB } from "@datastax/astra-db-ts";
-// import { AstraDBVectorStore } from "@langchain/community/vectorstores/astradb"
-// // Look at the documentation to correct these imports
-// import { OpenAIEmbeddings } from "@langchain/openai";
+import { AstraDB } from "@datastax/astra-db-ts";
+import { AstraDBVectorStore } from "@langchain/community/vectorstores/astradb"
+// Look at the documentation to correct these imports
+import { OpenAIEmbeddings } from "@langchain/openai";
 
-// const endpoint = process.env.ASTRA_DB_ENDPOINT || "";
-// const token = process.env.ASTRA_DB_APPLICATION_TOKEN || "";
-// const collection = process.env.ASTRA_DB_COLLECTION || "";
+const endpoint = process.env.ASTRA_DB_ENDPOINT || "";
+const token = process.env.ASTRA_DB_APPLICATION_TOKEN || "";
+const collection = process.env.ASTRA_DB_COLLECTION || "";
 
-// if (!token || !endpoint || !collection) {
-//     throw new Error(
-//         "Please set ASTRA_DB_ENDPOINT, ASTRA_DB_APPLICATION_TOKEN, and ASTRA_DB_COLLECTION environment variables."
-//     )
-// }
+if (!token || !endpoint || !collection) {
+    throw new Error(
+        "Please set ASTRA_DB_ENDPOINT, ASTRA_DB_APPLICATION_TOKEN, and ASTRA_DB_COLLECTION environment variables."
+    )
+}
 
-// export async function getVectorStore() {
-//     return AstraDBVectorStore.fromExistingIndex(
-//         new OpenAIEmbeddings({ modelName: "text-embedding-3-small" }),
-//         {
-//             token,
-//             endpoint,
-//             collection,
-//             collectionOptions: {
-//                 vector: {
-//                     dimension: 1536,
-//                     metric: "cosine"
-//                 }
-//             }
-//         }
-//     )
-// }
+export async function getVectorStore() {
+    return AstraDBVectorStore.fromExistingIndex(
+        new OpenAIEmbeddings({ modelName: "text-embedding-3-small" }),
+        {
+            token,
+            endpoint,
+            collection,
+            collectionOptions: {
+                vector: {
+                    dimension: 1536,
+                    metric: "cosine"
+                }
+            }
+        }
+    )
+}
 
-// export async function getEmbeddingsCollection() {
-//     return new AstraDB(token, endpoint).collection(collection);
-// }
+export async function getEmbeddingsCollection() {
+    return new AstraDB(token, endpoint).collection(collection);
+}
