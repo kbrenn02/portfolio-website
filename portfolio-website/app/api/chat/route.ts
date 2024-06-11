112130c835ea71c0452a7d56429df0bebfe35573
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-06-11 15:09:52 -0700

    Allow chatbot to understand chat history and respond appropriately to followup questions

--- portfolio-website/app/api/chat/route.ts
+++ portfolio-website/app/api/chat/route.ts
@@ -1,11 +1,13 @@
 'use server';
 
-import { LangChainStream, StreamingTextResponse } from "ai";
+import { LangChainStream, Message as VercelChatMessage, StreamingTextResponse } from "ai";
 import { ChatOpenAI } from "@langchain/openai";
-import { ChatPromptTemplate, PromptTemplate } from "@langchain/core/prompts"
+import { ChatPromptTemplate, PromptTemplate, MessagesPlaceholder } from "@langchain/core/prompts"
 import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
 import { getVectorStore } from "@/src/libs/astradb";
 import { createRetrievalChain } from "langchain/chains/retrieval";
+import { AIMessage, HumanMessage } from "@langchain/core/messages"
+import { createHistoryAwareRetriever } from "langchain/chains/history_aware_retriever";
 
 
 export async function POST(req: Request) {
@@ -13,6 +15,14 @@ export async function POST(req: Request) {
     try {
         const body = await req.json();
         const messages = body.messages;
+
+        const chatHistory = messages
+        .slice(0, -1)
+        .map((m: VercelChatMessage) => 
+        m.role === 'user' 
+            ? new HumanMessage(m.content)
+            : new AIMessage(m.content),
+        );
 
         const currentMessageContent = messages[messages.length - 1].content;
 
@@ -26,6 +36,30 @@ export async function POST(req: Request) {
             verbose: true,
         });
 
+        const rephrasingModel = new ChatOpenAI({
+            apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+            modelName: 'gpt-3.5-turbo',
+            verbose: true,
+        });
+
+        const retriever = (await getVectorStore()).asRetriever();
+
+        const rephrasePrompt = ChatPromptTemplate.fromMessages([
+            new MessagesPlaceholder("chat_history"),
+            ["user", "{input}"],
+            [
+                "user",
+                "Given the above conversation, generate a search query to look up in order to get information relevant to the current question. " +
+                "Don't leave out any relevant keywords. Only return the query and no other text.",
+            ],
+        ]);
+
+        const historyAwareRetrieverChain = await createHistoryAwareRetriever({
+            llm: rephrasingModel,
+            retriever,
+            rephrasePrompt
+        })
+
         const prompt = ChatPromptTemplate.fromMessages([
             [
                 "system",
@@ -35,6 +69,7 @@ export async function POST(req: Request) {
                 "Format your messages in markdown format.\n\n" +
                 "Context:\n{context}",
             ],
+            new MessagesPlaceholder("chat_history"),
             [
                 "user", "{input}"
             ]
@@ -49,11 +84,10 @@ export async function POST(req: Request) {
             documentSeparator: "\n-------------\n"
         })
         
-        const retriever = (await getVectorStore()).asRetriever();
 
         const retrievalChain = await createRetrievalChain({
             combineDocsChain,
-            retriever,
+            retriever: historyAwareRetrieverChain,
         });
 
         // Retrieval chain takes user input, turns it into a vector. It's then used in a similarity search in our
@@ -61,7 +95,8 @@ export async function POST(req: Request) {
         // createstuffdocumentchain which takes them and puts them into the context field of our prompt
 
         retrievalChain.invoke({
-            input: currentMessageContent
+            input: currentMessageContent,
+            chat_history: chatHistory
         })
 
         
