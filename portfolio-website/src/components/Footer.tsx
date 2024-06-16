4f562e49bd9821d7a1b6795ee80f6d8b8ff727b3
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-06-15 20:49:29 -0700

    Add mailto link and gmail logo

--- portfolio-website/src/components/Footer.tsx
+++ portfolio-website/src/components/Footer.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import github from '@/src/assets/github.svg';
 import linkedin from '@/src/assets/linkedin.svg';
 import instagram from '@/src/assets/instagram.svg';
+import gmail from '@/src/assets/gmail.svg'
 
 const Footer = () => {
   return (
@@ -14,23 +15,33 @@ const Footer = () => {
             <Link href="/privacy">Privacy</Link>
         </nav> 
         */}
-        <div className="flex items-center gap-6 bg-white px-4 py-2 rounded-full">
+        <div className="flex items-center gap-10 bg-white px-4 py-2 rounded-full text-2xl">
             <Link href="https://github.com/kbrenn02">
                 <Image 
                     src={github}
                     alt="Github logo"
+                    className='w-10 h-10'
                 />
             </Link>
             <Link href="https://www.linkedin.com/in/brennan-kevin/">
                 <Image 
                     src={linkedin}
                     alt="LinkedIn logo"
+                    className='w-10 h-10'
                 />
             </Link>
             <Link href="https://www.instagram.com/kev_brennan_/">
                 <Image 
                     src={instagram}
                     alt="Instagram logo"
+                    className='w-10 h-10'
+                />
+            </Link>
+            <Link href="mailto:kevinmbrennan08@gmail.com">
+                <Image 
+                    src={gmail}
+                    alt="Gmail logo"
+                    className='w-10 h-10'
                 />
             </Link>
         </div>
